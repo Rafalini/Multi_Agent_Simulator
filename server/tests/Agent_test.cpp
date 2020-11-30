0171bc069cef5728a50e71630d1c7bcf20730616
Rafalini <prajmik@gmail.com>
2020-11-30 23:47:35 +0100

    some tests added

---
+++ server/tests/Agent_test.cpp
@@ -0,0 +1,23 @@
+#define BOOST_TEST_MODULE Agent_test
+
+#include <boost/test/included/unit_test.hpp>
+#include "Agent.hpp"
+#include <memory>
+#include <sstream>
+
+using namespace boost::unit_test;
+
+BOOST_AUTO_TEST_CASE(AgentTest1)
+{
+      //std::shared_ptr<City> c1 = std::make_shared<City>(new City("Warszawa", 1, 2));
+      //std::shared_ptr<City> c2 = std::make_shared<City>(new City("Krakow", 3, 4));
+      //Agent a1(c1,c2, 5);
+    	//BOOST_CHECK_EQUAL(a1.getLoad(), 5);
+	    //BOOST_CHECK_EQUAL(a1.getOrigin(),c1);
+	    //BOOST_CHECK_EQUAL(a1.getDestination(),c2);
+}
+
+BOOST_AUTO_TEST_CASE(AgentTest2)
+{
+	   BOOST_CHECK_EQUAL(1,1);
+}
