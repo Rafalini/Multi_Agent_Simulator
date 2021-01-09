a75ecfffce8826066aea420cadeca327901cb9d0
Maciej Czyżkowski <m.czyzkowski@stud.elka.pw.edu.pl>
2021-01-09 21:39:56 +0100

    city unit tests

--- client/tests/tst_citytests.hpp
+++ client/tests/tst_citytests.hpp
@@ -21,10 +21,17 @@ public:
     }
 
 private slots:
-    void test_case1()
-    {
+    void jsonTests() {
+        double x = 0.1;
+        double y = 0.17;
+        QString name = "city_test";
+        City city(name, x, y);
+        QJsonObject comparableJson;
+        comparableJson["x"] = x;
+        comparableJson["y"] = y;
+        comparableJson["name"] = name;
+        comparableJson["id"] = city.getId();
+        QCOMPARE(comparableJson, city.toJson());
     }
-
-
 };
 
